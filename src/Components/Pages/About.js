d324832227b3860a4da716c89d18dd93e0fa7815
sourav mandal <souravmandal909p@gmail..com>
2023-02-25 17:11:28 +0530

    add about page code

--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import "../Pages/About.css"
+import { useState } from 'react';
+import img from "../../../src/Images/myimg.jpg";
 
 const About = () => {
+  const [about] = useState({
+    heading: "About Us",
+    titel: "About Me.",
+    aboutme: " Hey 👋 There I am Sourav Mandal a full stack web developer from India. I always like to use advanced technology for creating useful products and I like to use advanced programming languages & Frameworks like Python, JavaScript, React, nextjs, MongoDB, etc...  I have worked on react and nextjs. I love to learn new Technology for creating something new and amazing. I always try to learn something new and build something better and try to grow up by day by day. From 2021 I sharing my knowledge on social media like Twitter and Linkedin. I love to listening music 🎵🎶 when I am walking my favorite music band is ( THE SCORE ). I love to play guitar 🎸 I love dogs. usually, I read books when  I am free. I always try to help everyone. if you need any help with me about programming drop me a mail ✉ (sourav@souravmandal.com) "
+  })
   return (
-    <div>About</div>
+    <>
+     <div className="heading">
+      <h1>{about.heading}</h1>
+     </div>
+     <div className="about_main">
+      <p>{about.aboutme}</p>
+     </div>
+    </>
   )
 }
 
--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -8,84 +8,89 @@ import { AiFillGithub } from "react-icons/ai";
 import { AiFillInstagram } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import Typewriter from "typewriter-effect";
+import About from "./About";
 
 const Home = () => {
-
   const [state] = useState({
     titel: "Hello, It's Me",
     myname: "Sourav Mandal",
-  })
-
+  });
 
   return (
-    <div className="hero">
-      <div className="text">
-        <h1>{state.titel}</h1>
-        <h2>{state.myname}</h2>
-        <Typewriter
-        options={{autoStart: true,
-        loop: true,
-      delay: 40,
-    strings: [
-      "I am a Web Developer",
-      "I am a Full Stact Web Deeveloper",
-      "I am a Youtuber",
-      "I am a Content Creator",
-      "I am a Mentor"
-    ]
-  }}
-  />
-        <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br />  Eum, voluptate. Lorem ipsum dolor sit amet.
-        </p>
+    <>
+      <div className="hero">
+        <div className="text">
+          <h1>{state.titel}</h1>
+          <h2>{state.titel}</h2>
+          <Typewriter
+            options={{
+              autoStart: true,
+              loop: true,
+              delay: 40,
+              strings: [
+                "I am a Web Developer",
+                "I am a Full Stact Web Deeveloper",
+                "I am a Youtuber",
+                "I am a Content Creator",
+                "I am a Mentor",
+              ],
+            }}
+          />
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br />{" "}
+            Eum, voluptate. Lorem ipsum dolor sit amet.
+          </p>
 
-        <div className="socile_media">
-          <a
-            href="https://twitter.com/souravcode"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <AiOutlineTwitter />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/souravcode/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <AiFillLinkedin />
-          </a>
-          <a
-            href="https://www.instagram.com/souravcode/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <AiFillInstagram />
-          </a>
-          <a
-            href="https://www.youtube.com/channel/UC34QqM-x6iaOD9ikoch9FAg"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <AiFillYoutube />
-          </a>
-          <a
-            href="https://github.com/souravcodeweb"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <AiFillGithub />
-          </a>
+          <div className="socile_media">
+            <a
+              href="https://twitter.com/souravcode"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiOutlineTwitter />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/souravcode/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiFillLinkedin />
+            </a>
+            <a
+              href="https://www.instagram.com/souravcode/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiFillInstagram />
+            </a>
+            <a
+              href="https://www.youtube.com/channel/UC34QqM-x6iaOD9ikoch9FAg"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiFillYoutube />
+            </a>
+            <a
+              href="https://github.com/souravcodeweb"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiFillGithub />
+            </a>
+          </div>
+          <div className="btn">
+            <Link href="/7" download="#">
+              Download CV
+            </Link>
+          </div>
         </div>
-        <div className="btn">
-          <Link href="/7" download="#">
-            Download CV
-          </Link>
+        <div className="img">
+          <img src={img} alt="logo" />
         </div>
       </div>
-      <div className="img">
-        <img src={img} alt="logo" />
-      </div>
-    </div>
+
+      <About/>
+    </>
   );
 };
 
